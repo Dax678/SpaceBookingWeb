31d392a9b55edf9de18cad54ecf2cd20c883e34e
Dax678 <dmiskowi@student.wszib.edu.pl>
2023-04-09 19:19:20 +0200

    Added OpenAPI

--- src/main/java/com/example/spacebookingweb/Configuration/Swagger/SwaggerConfig.java
+++ src/main/java/com/example/spacebookingweb/Configuration/Swagger/SwaggerConfig.java
@@ -1,16 +1,15 @@
 package com.example.spacebookingweb.Configuration.Swagger;
 
-import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
- import org.springdoc.core.SpringDocConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
@@ -18,7 +17,6 @@ public class SwaggerConfig {
                         .title("Moja aplikacja Spring Boot z dokumentacją OpenAPI")
                         .description("Ta aplikacja to przykład, jak korzystać z OpenAPI w Spring Boot")
                         .version("1.0.0")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
     }
-
 }
